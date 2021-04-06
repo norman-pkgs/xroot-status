8a0fc9c8ee685b401a0af3a208e8ba6017bd5b9e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-04-06 12:42:15 +0200

    added brightness and wlan stati and modified install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-cp bin/* ~/.local/bin -r
+[[ ! -f ~/.local/bin/xroot-status-update ]] && cp bin/xroot-status-update ~/.local/bin 
+cp bin/xroot-status-loop ~/.local/bin 
+cp bin/xroot-status-stop ~/.local/bin 
+cp bin/xroot-status-start ~/.local/bin 
+cp bin/xroot-status-restart ~/.local/bin 
+cp bin/xroot-panels ~/.local/bin -r
