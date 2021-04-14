fd9522801be2ec8070ed4bc16c9c29f7cffbc142
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-04-14 15:34:52 +0200

    removed install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-[[ ! -f ~/.local/bin/xroot-status-update ]] && cp bin/xroot-status-update ~/.local/bin 
-cp bin/xroot-status-loop ~/.local/bin 
-cp bin/xroot-status-stop ~/.local/bin 
-cp bin/xroot-status-start ~/.local/bin 
-cp bin/xroot-status-restart ~/.local/bin 
-cp bin/xroot-panels ~/.local/bin -r
